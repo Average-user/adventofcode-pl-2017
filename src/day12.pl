05743dbc3351cc1e7529fe3b6060182737db48ad
Average-user <lucaspolymeris@gmail.com>
2017-12-12 23:29:34 -0300

    Cleaning Day 12

--- src/day12.pl
+++ src/day12.pl
@@ -1,7 +1,7 @@
 :- use_module(tools).
 
 connected_to(Edges, Xs, R) :-
-  member((X,Y), Edges), member(X, Xs), not(member(Y, Xs))
+ member(X, Xs), member((X,Y), Edges), not(member(Y, Xs))
    -> Xs1 = [Y|Xs], connected_to(Edges, Xs1, R)
     ; R = Xs.
 
@@ -11,20 +11,12 @@ day12a(A) :-
   length(Connected0, A).
 
 
-no_with(_,  [],         []) :- !.
-no_with(Es, [(A, B)|T], R)  :-
-  no_with(Es, T, Xs),
-  ((member(A, Es) ; member(B, Es))
-   -> R = Xs
-    ; R = [(A, B)| Xs]).
-
-groups(_ ,        [],    Ac, Ac) :- !.
+groups([],        _,     Ac, Ac) :- !.
 groups([N|Nodes], Edges, Ac, R)  :-
   connected_to(Edges, [N], Cs),
-  no_with(Cs, Edges, NEdges),
   findall(X, (member(X, Nodes), not(member(X, Cs))), NNodes),
   NAc is Ac+1,
-  groups(NNodes, NEdges, NAc, R).
+  groups(NNodes, Edges, NAc, R).
 
 nodes_of(Edges, Nodes) :-
   maplist(fst, Edges, F), maplist(snd, Edges, S),
@@ -48,8 +40,7 @@ all_relations(A, [R|Rs], Xs) :-
   all_relations(A, Rs, Xs1),
   Xs = [(A, R), (R, A)| Xs1].
 
-to_relations([H,T], Rs) :-
-  all_relations(H, T, Rs).
+to_relations([H,T], Rs) :- all_relations(H, T, Rs).
 
 connections(X, Xs) :-
   split_string(X, "<->", "<->", [H,T]),
