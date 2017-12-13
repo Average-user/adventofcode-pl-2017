2291ba6873e1c334161c5db665f8b81aaedb9706
Average-user <lucaspolymeris@gmail.com>
2017-12-12 21:53:32 -0300

    More Refactoring Day 11

--- src/day11.pl
+++ src/day11.pl
@@ -1,25 +1,5 @@
 :- use_module(tools).
 
-distance([],    X,          X)  :- !.
-distance([n|T], v(X, Y, Z), Vs) :-
-  Y1 is Y+1, Z1 is Z-1, distance(T, v(X, Y1, Z1), Vs), !.
-distance([s|T], v(X, Y, Z), Vs) :-
-  Y1 is Y-1, Z1 is Z+1, distance(T, v(X, Y1, Z1), Vs), !.
-distance([ne|T], v(X, Y, Z), Vs) :-
-  X1 is X+1, Z1 is Z-1, distance(T, v(X1, Y, Z1), Vs), !.
-distance([sw|T], v(X, Y, Z), Vs) :-
-  X1 is X-1, Z1 is Z+1, distance(T, v(X1, Y, Z1), Vs), !.
-distance([nw|T], v(X, Y, Z), Vs) :-
-  X1 is X-1, Y1 is Y+1, distance(T, v(X1, Y1, Z), Vs), !.
-distance([se|T], v(X, Y, Z), Vs) :-
-  X1 is X+1, Y1 is Y-1, distance(T, v(X1, Y1, Z), Vs), !.
-
-day11a(A) :-
-  from_file("Inputs/day11.txt", F),
-  distance(F, v(0,0,0), v(X,Y,Z)),
-  A is (abs(X)+abs(Y)+abs(Z)) div 2.
-
-
 new_coor(n , 0, 1, -1).
 new_coor(s , 0, -1, 1).
 new_coor(ne, 1, 0, -1).
@@ -27,10 +7,23 @@ new_coor(sw, -1, 0, 1).
 new_coor(nw, -1, 1, 0).
 new_coor(se, 1, -1, 0).
 
+change_coor(D, v(X, Y, Z), v(NX, NY, NZ)) :-
+  new_coor(D, A, B, C),
+  NX is X+A, NY is Y+B, NZ is Z+C.
+
+distance([],    X,  X)  :- !.
+distance([D|T], X,  Vs) :-
+  change_coor(D, X, NX), distance(T, NX, Vs).
+
+day11a(A) :-
+  from_file("Inputs/day11.txt", F),
+  distance(F, v(0,0,0), v(X,Y,Z)),
+  A is (abs(X)+abs(Y)+abs(Z)) div 2.
+
+
 maxdis([],    _,          Ac, Ac) :- !.
-maxdis([E|T], v(X, Y, Z), Ac, Vs) :-
-  new_coor(E, A, B, C),
-  NX is X+A, NY is Y+B, NZ is Z+C,
+maxdis([D|T], v(X, Y, Z), Ac, Vs) :-
+  change_coor(D, v(X, Y, Z), v(NX, NY, NZ)),
   AE is (abs(NX)+abs(NY)+abs(NZ)) div 2,
   Ac1 = [AE|Ac],
   maxdis(T, v(NX, NY, NZ), Ac1, Vs), !.
@@ -39,7 +32,6 @@ day11b(B) :-
   from_file("Inputs/day11.txt", F),
   maxdis(F, v(0,0,0), [], Ac),
   max_list(Ac, B).
-
 
 %% Reading File (formating the input)
 from_file(Path, F) :-
