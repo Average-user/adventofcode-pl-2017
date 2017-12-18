1ed81203bc92901bab0182936eba7e52d0322d24
Average-user <lucaspolymeris@gmail.com>
2017-12-18 19:56:12 -0300

    Day 18 part B

--- src/day18.pl
+++ src/day18.pl
@@ -40,14 +40,41 @@ run(Ins, Ix, Vars, Rec, L)   :-
      -> NIx is Ix+VB ; NIx is Ix+1),
    (eval(I, Vars, NVars),! ; NVars = Vars),
    run(Ins, NIx, NVars, NRec, L)).
+
+nextStateH(snd(X), Ix, Vs, Q, NIx, Vs, Q, S) :-
+  get(X, Vs, VX), S = VX, succ(Ix, NIx), !.
+
+nextStateH(rcv(X), Ix, Vs, Q, NIx, NVs, NQ, nil) :-
+  Q = [H|NQ], get(H, Vs, VH), eval(set(X, VH), Vs, NVs), NIx is Ix+1
+  ; NVs = Vs, NQ = Q, NIx = Ix.
+
+nextStateH(jgz(A, B), Ix, Vs, Q, NIx, Vs, Q, nil) :-
+  get(A, Vs, Va), Va > 0, get(B, Vs, Vb), NIx is Ix+Vb, ! ; succ(Ix, NIx).
+
+nextStateH(X, Ix, Vs, Q, NIx, NVs, Q, nil) :-
+  eval(X, Vs, NVs), succ(Ix, NIx), !.
+
+nextState(P, Ix, Vs, Q, NIx, NVs, NQ, S) :-
+  elem_of(P, Ix, I), nextStateH(I, Ix, Vs, Q, NIx, NVs, NQ, S).
+  
+coordinate(P, Vs1, Vs2, Ix1, Ix2, Q1, Q2, Ac, R) :-
+  nextState(P, Ix1, Vs1, Q1, NIx1, NVs1, NQ1, S1),
+  nextState(P, Ix2, Vs2, Q2, NIx2, NVs2, NQ2, S2),
+  ( (Ix1 = NIx1, Ix2 = NIx2) -> R is Ac ;
+    (S1 = 'nil' -> FQ2 = NQ2 ; append(NQ2, [S1], FQ2)),
+    (S2 = 'nil' -> FQ1 = NQ1, NAc = Ac ; append(NQ1, [S2], FQ1), NAc is Ac+1),
+    coordinate(P, NVs1, NVs2, NIx1, NIx2, FQ1, FQ2, NAc, R)).
+
   
 day18a(A) :-
   from_file("Inputs/day18.txt", (Vs, Ins)),
   run(Ins, 0, Vs, [], [A|_]).
 
 day18b(B) :-
-  from_file("Inputs/day18.txt", B).
-
+  from_file("Inputs/day18.txt", (Vs, Ins)),
+  member((p, X), Vs), select((p, X), Vs, NVs), !, Vs1 = [(p, 1)|NVs],
+  coordinate(Ins, Vs, Vs1, 0, 0, [], [], 0, B), !.
+  
 % Formating Input:
 from_file(Path, F) :-
   file_to_lines(Path, Lines),
@@ -83,5 +110,3 @@ vars([H|Xs], Vars) :-
   maplist(string_to_atom, S, [_|R]),
   findall(X, (member(X, R), atom_codes(X, [C|_]), member(C, Abc)), Vs),
   Vars = [Vs|Vars1].
-    
-  
